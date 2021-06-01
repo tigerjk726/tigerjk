bd50e386931c9ced45192b72023e49b0c14ab515
tigerjk726 <78900198+tigerjk726@users.noreply.github.com>
2021-06-01 10:59:21 +0900

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,40 @@
+# total_dic = {}
+
+# while True:
+#     question = input("질문을 하세요 : ")
+#     if question == "q":
+#         break
+#     else:
+#         total_dic[question] = ""
+
+# for i in total_dic:
+#     print(i)
+#     answer = input("답변을 하세요 : ")
+#     total_dic[i] = answer
+
+# print(total_dic)
+
+
+# total_list = []
+
+# while True:
+#     question = input("질문을 하세요 : ")
+#     if question == "q":
+#         break
+#     else:
+#         total_list.append({"질문" : question, "답변" : ""})
+
+# for i in total_list:
+#     print(i["질문"])
+#     answer = input("답변을 하세요: ")
+#     i["답변"] = answer
+
+# print(total_list)
+
+# list = []
+
+# list.append({"범중" : "천재", "다롬" : 2})
+
+
+# print(list[0]["범중"])
+
